1b958973dbbfbdce839dc66bf98db746f50c03f6
abelrugue <abelrugue@gmail.com>
2026-07-06 17:49:00 +0200

    first commit

---
+++ web/js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+    "https://qmvpyhlkhcbyuytxxvub.supabase.co",
+    "sb_publishable_MqTGnFQIoSmY1sAsqNCWXA_GRALJBw3"
+);
