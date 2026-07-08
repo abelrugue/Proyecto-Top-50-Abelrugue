af3d1af513ecde7e0557980e192de3913a6bcaad
abelrugue <abelrugue@gmail.com>
2026-07-09 01:31:30 +0200

    btn click

--- web/js/entradas_artista.js
+++ web/js/entradas_artista.js
@@ -3,12 +3,16 @@
 import { cancionRenderer } from "./renderers/render_entradas.js";
 import { messageRenderer } from './renderers/messages.js';
 import { sessionManager } from "./utils/session.js";
+import { supabase } from "./supabase.js";
 
 async function main() {
     try {
 
-        let bodyDiv = document.getElementById("body");
-        bodyDiv.innerHTML += await asCardGallery();
+        let btn = document.getElementById("btn");
+
+        btn.addEventListener("click", asCardGallery);
+
+        
 
     } catch (err) {
         messageRenderer.showErrorMessage(err);
@@ -20,28 +24,29 @@ async function main() {
 
 async function asCardGallery() {
 
-        let inputname = document.getElementById("name-input");
+    let inputname = document.getElementById("name-input");
 
-        let btn = document.getElementById("btn");
-
-        btn.addEventListener("click", async () => {
-            let { data, error } = await supabase
-                .from("vista_artista_canciones")
-                .select("*")
-                .eq("nombre", inputname.value.trim())
-                .order("fecha_debut");
-
-            let html = '';
-            for (let cancion of data) {
-                html += cancionRenderer.asCard(cancion);
-            }
-            
-        });
-
-        return html;
+    let bodyDiv = document.getElementById("body");
+        
 
 
+    let { data, error } = await supabase
+        .from("vista_artista_canciones")
+        .select("*")
+        .eq("nombre", inputname.value.trim())
+        .order("fecha_debut");
+
+    let html = '';
+    for (let cancion of data) {
+        html += cancionRenderer.asCard(cancion);
     }
+
+
+
+    bodyDiv.innerHTML = html;
+
+
+}
 
 
 
