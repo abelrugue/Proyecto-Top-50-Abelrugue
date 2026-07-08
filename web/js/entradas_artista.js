771415c7405b0b4e694929c8d4e7a4937a0b58de
abelrugue <abelrugue@gmail.com>
2026-07-09 01:21:35 +0200

    cambio de sitio ascardgallery

--- web/js/entradas_artista.js
+++ web/js/entradas_artista.js
@@ -8,7 +8,7 @@ async function main() {
     try {
 
         let bodyDiv = document.getElementById("body");
-        bodyDiv.innerHTML += await cancionRenderer.asCardGallery();
+        bodyDiv.innerHTML += await asCardGallery();
 
     } catch (err) {
         messageRenderer.showErrorMessage(err);
@@ -17,6 +17,31 @@ async function main() {
 
 
 }
+
+async function asCardGallery() {
+
+        let inputname = document.getElementById("name-input");
+
+        let btn = document.getElementById("btn");
+
+        btn.addEventListener("click", async () => {
+            let { data, error } = await supabase
+                .from("vista_artista_canciones")
+                .select("*")
+                .eq("nombre", inputname.value.trim())
+                .order("fecha_debut");
+
+            let html = '';
+            for (let cancion of data) {
+                html += cancionRenderer.asCard(cancion);
+            }
+            
+        });
+
+        return html;
+
+
+    }
 
 
 
--- web/js/renderers/render_entradas.js
+++ web/js/renderers/render_entradas.js
@@ -29,31 +29,6 @@ const cancionRenderer = {
 
 </tr>`;
         return html;
-    },
-
-    asCardGallery: async function () {
-
-        let inputname = document.getElementById("name-input");
-
-        let btn = document.getElementById("btn");
-
-        btn.addEventListener("click", async () => {
-            let { data, error } = await supabase
-                .from("vista_artista_canciones")
-                .select("*")
-                .eq("nombre", inputname.value.trim())
-                .order("fecha_debut");
-
-            let html = '';
-            for (let cancion of data) {
-                html += this.asCard(cancion);
-            }
-            
-        });
-
-        return html;
-
-
     }
 };
 export { cancionRenderer };
