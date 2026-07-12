c240a29134e1fc574b23d2961ee5400463efcddb
abelrugue <abelrugue@gmail.com>
2026-07-12 20:00:13 +0200

    console

--- web/js/añadir_semana.js
+++ web/js/añadir_semana.js
@@ -4,13 +4,13 @@ import { messageRenderer } from './renderers/messages.js';
 
 async function main() {
     try {
-        console.log("1");
+        
         let btn_buscar_artistas = document.getElementById("btn-buscar-artistas");
-        console.log("2", btn_buscar_artistas);
+        
         if (btn_buscar_artistas) {
             btn_buscar_artistas.addEventListener("click", buscaArtistas);
         }
-        console.log("3");
+        
 
     } catch (err) {
         messageRenderer.showErrorMessage(err);
@@ -19,18 +19,20 @@ async function main() {
 
 async function buscaArtistas() {
 
-    console.log("4");
+    
 
     let textarea = document.getElementById("lista-input");
-    console.log("5", textarea);
+    
     let texto = textarea.value;
-    console.log("6");
+    
 
     const titulos = texto
         .trim()
         .split("\n")
         .map(l => l.trim())
         .filter(Boolean);
+
+    console.log(JSON.stringify({ titulos }, null, 2));
 
     const { data, error } = await supabase.functions.invoke(
         "buscar-canciones",
