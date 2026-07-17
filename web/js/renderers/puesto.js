0f0780bd83c0d17d396c737592829030265a3a41
abelrugue <abelrugue@gmail.com>
2026-07-17 20:03:54 +0200

    col3

--- web/js/renderers/puesto.js
+++ web/js/renderers/puesto.js
@@ -89,7 +89,7 @@ const puestoRenderer = {
 
                     </div>
                 </div>
-                <div class="col-2 col-md-3 d-flex align-items-center">
+                <div class="col-3 col-md-3 d-flex align-items-center">
                     <div class="card-body">
                         <div class="d-flex flex-column flex-md-row gap-2 gap-md-4 text-center w-100 ">
                             <div>
