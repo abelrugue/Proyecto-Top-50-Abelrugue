cdd08d331d6cbe41a7a1f472c765f832a5a51a23
abelrugue <abelrugue@gmail.com>
2026-07-20 15:53:40 +0200

    movil

--- web/js/song_details.js
+++ web/js/song_details.js
@@ -45,14 +45,14 @@ function asCard(cancion) {
     let html = `
             <br>
             <div class="card mb-2 p-1">
-                <div class="row g-1 align-items-center m-0">
+                <div class="row g-1 align-items-center m-0 d-none d-md-flex">
 
-                    <div class="col-4 col-md-4">
+                    <div class="col-md-4">
                         <img src="${cancion.portada_url && cancion.portada_url !== 'NO_ENCONTRADA' ? cancion.portada_url : 'https://quinpart.com/imgs/placeholder.svg'}"
                         class="img-fluid rounded w-100 h-100 object-fit-cover">
                     </div>
 
-                    <div class="col-8 col-md-8 d-flex align-items-center">
+                    <div class="col-md-8 d-flex align-items-center">
                         <div class="card-body">
                             <h1 class="card-title">${cancion.titulo}</h1>
                             <h4 class="card-text">${cancion.artistas}</h4>
@@ -87,6 +87,83 @@ function asCard(cancion) {
                         </div>
                     </div>
                 </div>
+
+                <div class="d-md-none">
+
+                    <div class="row g-2 align-items-center">
+
+                        <div class="col-4">
+                            <img src="${cancion.portada_url && cancion.portada_url !== 'NO_ENCONTRADA' ? cancion.portada_url : 'https://quinpart.com/imgs/placeholder.svg'}"
+                            class="img-fluid rounded w-100 object-fit-cover">
+                        </div>
+
+                        <div class="col-8">
+                            <h3 class="card-title mb-1">${cancion.titulo}</h3>
+                            <h5 class="card-text">${cancion.artistas}</h5>
+                        </div>
+
+                    </div>
+
+                    <ul class="mt-2 mb-1">
+                        <li>
+                            <p class="card-text">Max: <i class="fa-solid fa-trophy"></i> ${cancion.peak}${repeticion}</p>
+                        </li>
+                        <li>
+                            <p class="card-text">Sem: <i class="fa-solid fa-calendar"></i> ${cancion.sem}</p>
+                        </li>
+                        <li>
+                            <p class="card-text">Punt: ${cancion.puntuacion}</p>
+                        </li>
+                        <li>
+                            <p class="card-text">Recorrido: ${cancion.recorrido}</p>
+                        </li>
+                        <li>
+                            <p class="card-text">Semanas top 5: ${cancion.top_5}</p>
+                        </li>
+                        <li>
+                            <p class="card-text">Semanas top 10: ${cancion.top_10}</p>
+                        </li>
+                        <li>
+                            <p class="card-text">Fecha debut: ${cancion.fecha_debut}</p>
+                        </li>
+                        <li>
+                            <p class="card-text">Fecha peak: ${cancion.fecha_peak}</p>
+                        </li>
+                    </ul>
+
+                </div>
+
+
+
+            </div>
+
+            <div class="card mb-2 p-1">
+
+                <!-- MÓVIL -->
+                <div class="d-md-none">
+
+                    <div class="row g-2 align-items-center">
+
+                        <div class="col-4">
+                            <img src="${cancion.portada_url && cancion.portada_url !== 'NO_ENCONTRADA'
+                                ? cancion.portada_url
+                                : 'https://quinpart.com/imgs/placeholder.svg'}"
+                                class="img-fluid rounded w-100 object-fit-cover">
+                        </div>
+
+                        <div class="col-8">
+                            <h3 class="card-title mb-1">${cancion.titulo}</h3>
+                            <h5 class="card-text">${cancion.artistas}</h5>
+                        </div>
+
+                    </div>
+
+                    <ul class="mt-2 mb-1">
+                        ...
+                    </ul>
+
+                </div>
+
             </div>
             `;
 
