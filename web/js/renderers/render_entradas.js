55cc0f1c1fc56325419fbf83f31f89b83fb5f2bd
abelrugue <abelrugue@gmail.com>
2026-07-20 20:15:14 +0200

    ancho cancion

--- web/js/renderers/render_entradas.js
+++ web/js/renderers/render_entradas.js
@@ -16,15 +16,19 @@ const cancionRenderer = {
             num1 = `style="background-color: rgb(255, 60, 60);"`;
         }
 
+        let repeticion = "";
+        if (cancion.numeros_1 > 1){
+            repeticion = `x${cancion.numeros_1}`
+        }
+
         let html = `<tr ${num1}>
         <td scope="col" class="portada-cell"><img src="${cancion.portada_url && cancion.portada_url !== 'NO_ENCONTRADA' ? cancion.portada_url : 'https://quinpart.com/imgs/placeholder.svg'}"
          class="rounded portada-img"></td>
 <td scope="col">${cancion.titulo}</td>
 <td scope="col" class="artistas-cell">${cancion.artistas}</td>
-<td scope="col">${cancion.peak}</td>
+<td scope="col">${cancion.peak}${repeticion}</td>
 <td scope="col">${cancion.sem}</td>
 <td scope="col">${cancion.puntuacion}</td>
-<td scope="col">${cancion.numeros_1}</td>
 <td scope="col">${cancion.top_5}</td>
 <td scope="col">${cancion.top_10}</td>
 <td scope="col">${cancion.fecha_debut}</td>
