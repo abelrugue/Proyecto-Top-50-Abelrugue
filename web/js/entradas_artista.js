292305f6fd7ef48b98fc93915945b3bfcdaffa74
abelrugue <abelrugue@gmail.com>
2026-07-21 21:04:49 +0200

    buscar entarads n1

--- web/js/entradas_artista.js
+++ web/js/entradas_artista.js
@@ -7,7 +7,7 @@ import { supabase } from "./supabase.js";
 
 async function main() {
     try {
-
+        /*
         let btn = document.getElementById("btn");
         if (btn) {
             btn.addEventListener("click", asCardGallery);
@@ -34,9 +34,39 @@ async function main() {
                     asCardGallery_n1();
                 }
             });
+        }*/
+
+        let form = document.getElementById("entradas-form");
+        if (form) {
+            form.addEventListener("submit", e => {
+                e.preventDefault();
+                buscar();
+            });
+
+            const params = new URLSearchParams(window.location.search);
+
+            const artista = params.get("name") ?? "";
+
+            document.getElementById("name-input").value = artista;
+
+            await asCardGallery(artista);
         }
 
+        let formn1 = document.getElementById("n1-form");
+        if (formn1) {
+            formn1.addEventListener("submit", e => {
+                e.preventDefault();
+                buscarn1();
+            });
 
+            const params = new URLSearchParams(window.location.search);
+
+            const artista = params.get("name") ?? "";
+
+            document.getElementById("name-input-n1").value = artista;
+
+            await asCardGallery_n1(artista);
+        }
 
     } catch (err) {
         messageRenderer.showErrorMessage(err);
@@ -46,14 +76,37 @@ async function main() {
 
 }
 
-async function asCardGallery() {
-    let inputname = document.getElementById("name-input");
+function buscar() {
+    const artista = document.getElementById("name-input").value;
+    if (!artista) return;
+
+    history.pushState({}, "", `?name=${artista}`);
+
+    const lista = document.getElementById("body");
+    lista.replaceChildren();
+
+    asCardGallery(artista);
+}
+
+function buscarn1() {
+    const artista = document.getElementById("name-input-n1").value;
+    if (!artista) return;
+
+    history.pushState({}, "", `?name=${artista}`);
+
+    const lista = document.getElementById("body-n1");
+    lista.replaceChildren();
+
+    asCardGallery_n1(artista);
+}
+
+async function asCardGallery(artista) {
     let bodyDiv = document.getElementById("body");
 
     let { data, error } = await supabase
         .from("vista_artista_canciones")
         .select("*")
-        .eq("nombre", inputname.value.trim())
+        .eq("nombre", artista.trim())
         .order("fecha_debut");
     let html = '';
     for (let cancion of data) {
@@ -63,14 +116,13 @@ async function asCardGallery() {
     bodyDiv.innerHTML = html;
 }
 
-async function asCardGallery_n1() {
-    let inputname = document.getElementById("name-input-n1");
+async function asCardGallery_n1(artista) {
     let bodyDiv = document.getElementById("body-n1");
 
     let { data, error } = await supabase
         .from("vista_artista_canciones_num_1")
         .select("*")
-        .eq("nombre", inputname.value.trim())
+        .eq("nombre", artista.trim())
         .order("fecha_peak");
     let html = '';
     for (let cancion of data) {
