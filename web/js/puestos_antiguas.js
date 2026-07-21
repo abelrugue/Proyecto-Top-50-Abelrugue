cde540bd1084cf46708ee2afc54704a56d2b98ec
abelrugue <abelrugue@gmail.com>
2026-07-21 14:38:43 +0200

    listas antiguas

--- web/js/puestos_antiguas.js
+++ web/js/puestos_antiguas.js
@@ -4,22 +4,30 @@ import { supabase } from "./supabase.js";
 import { messageRenderer } from './renderers/messages.js';
 async function main() {
     try {
-        let content = document.getElementById("content");
+        let content = document.getElementById("content_antiguas");
+
+        let fecha = '2026-07-05';
 
         let { data, error } = await supabase
-            .from("vista_lista_actual")
-            .select("*");
+            .rpc("vista_lista_antigua", {
+                fecha_consulta: fecha
+            });
+
         let { data: hitos, error: error2 } = await supabase
-            .from("vista_hitos_semana")
-            .select("*")
+            .rpc("vista_hitos_semana", {
+                fecha_consulta: fecha
+            })
             .single();
         let { data: rdps, error: error3 } = await supabase
-            .from("vista_record_permanencia")
-            .select("*");
+            .rpc("vista_record_permanencia", {
+                fecha_consulta: fecha
+            });
         let { data: salidas, error: error4 } = await supabase
-            .from("vista_salidas")
-            .select("*");
+            .rpc("vista_salidas", {
+                fecha_consulta: fecha
+            });
         content.appendChild(galleryRenderer.asCardGallery(data, hitos, rdps, salidas));
+        document.getElementById("title-lista-antigua").innerHTML = `Lista Top 50 de Abelrugue del ${fecha}`;
     } catch (err) {
         messageRenderer.showErrorMessage(err);
     }
